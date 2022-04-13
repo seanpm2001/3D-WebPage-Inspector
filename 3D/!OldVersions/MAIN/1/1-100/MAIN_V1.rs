311cafe777b4126c8adc7f2df216d15de823ebab
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-04-13 15:07:01 -0700

    MAIN_V1.rs
    An archive of the first version of the main script file for this project.

---
+++ 3D/!OldVersions/MAIN/1/1-100/MAIN_V1.rs
@@ -0,0 +1,65 @@
+// Start of script
+// 3D WebPage Inspector
+// Written in Rust
+fn main() {
+  return inspectElement();
+  return threeDInspectButton();
+  return 0;
+  break;
+}
+fn inspectElement() {
+  // Inspect element is the mode this project runs in. Press F12 with the addon installed, and there will be a 3D inspector button
+  // Defining all HTML tags
+  // Note: A blank space is left open, as sometimes, another attribute is placed there
+  let detailsTag = "<details\n>\n</details>";
+  let h1Tag = "<H1\n>\n</H1>";
+  let h2Tag = "<H2\n>\n</H2>";
+  let h3Tag = "<H3\n>\n</H3>";
+  let h4Tag = "<H4\n>\n</H4>";
+  let h5Tag = "<H5\n>\n</H5>";
+  let h6Tag = "<H6\n>\n</H6>";
+  let pTag = "<p\n>\n</p>";
+  let bTag = "<b\n>\n</b>";
+  let iTag = "<i\n>\n</i>";
+  let uTag = "<u\n>\n</u>";
+  let sTag = "<s\n>\n</s>";
+  let aTag = "<a\n>\n</a>";
+  let empTag = "<emp\n>\n</emp>";
+  let brTag = "<br\n>\n</br>";
+  let htmlTag = "<html\n>\n</html>";
+  let bodyTag = "<body\n>\n</body>";
+  let headTag = "<head\n>\n</head>";
+  let titleTag = "<title\n>\n</title>";
+  let marqueeTag = "<marquee\n>\n</marquee>";
+  let blinkTag = "<blink\n>\n</blink>";
+  let imgTag = "<img\n>\n</img>";
+  let videoTag = "<video\n>\n</video>";
+  let audioTag = "<audio\n>\n</audio>";
+  let summaryTag = "<summary\n>\n</summary>";
+  let divTag = "<div\n>\n</div>";
+  let HRTag = "<hr\n>\n</hr>";
+  let scriptTag = "<script\n>\n</script>";
+  let styleTag = "<style\n>\n</style>";
+  let articleTag = "<article\n>\n</article>";
+  let sectionTag = "<section\n>\n</section>";
+  let footerTag = "<footer\n>\n</footer>";
+  let headerTag = "<header\n>\n</header>";
+  let metaTag = "<meta\n>\n</meta>";
+  let mathTag = "<math\n>\n</math>";
+  let xmlTag = "<xml\n>\n</xml>";
+  let linkTag = "<link\n>\n</link>";
+  let buttonTag = "<button\n>\n</button>";
+  let progressTag = "<progress\n>\n</progress>";
+  break;
+  // I don't have the time to list every HTML5 tag right now
+}
+fn threeDInspectButton() {
+  return inspectElement();
+  let button1 = "/3D/Graphics/Button/1.svg";
+  break;
+}
+// File info
+// File type: Rust source file (*.rs)
+// File version: 1 (2022, Wednesday, April 13th at 3:06 pm PST)
+// Line count (including blank lines and compiler line): 66
+// End of script
